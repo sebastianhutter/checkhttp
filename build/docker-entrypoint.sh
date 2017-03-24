37ec2123b49f1c18e38792f8e88f5b93178b5acf
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-03-24 18:19:19 +0100

    Add first commit with vault secret

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# if vault is enabled try to access it and get the username and password
+if [ -n "$VAULT_SERVER"]; then
+  ACCESS_TOKEN=$(curl -X POST \
+    -d "{\"role_id\":\"${VAULT_ROLE_ID}\",\"secret_id\":\"$VAULT_SECRET_ID\"}" \
+    ${VAULT_SERVER}/v1/auth/approle/login | jq -r .auth.client_token)
+
+  json=$(curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" \
+    ${VAULT_SERVER}/v1/${VAULT_SECRET_HTTP_CREDENTIALS})
+
+fi
+
 exec python /app/checkhttp.py
