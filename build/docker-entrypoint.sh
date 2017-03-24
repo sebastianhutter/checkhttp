c73ba1082490f7ce401b30ec848574b1e3aeb5a9
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-03-24 23:32:30 +0100

    Add credentials via vault

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # if vault is enabled try to access it and get the username and password
-if [ -n "$VAULT_SERVER"]; then
+if [ -n "$VAULT_SERVER" ]; then
   ACCESS_TOKEN=$(curl -X POST \
     -d "{\"role_id\":\"${VAULT_ROLE_ID}\",\"secret_id\":\"$VAULT_SECRET_ID\"}" \
     ${VAULT_SERVER}/v1/auth/approle/login | jq -r .auth.client_token)
@@ -9,6 +9,12 @@ if [ -n "$VAULT_SERVER"]; then
   json=$(curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" \
     ${VAULT_SERVER}/v1/${VAULT_SECRET_HTTP_CREDENTIALS})
 
+  username=$(echo $json | jq -r .data.username)
+  password=$(echo $json | jq -r .data.password)
+
+  CLI_ENV=""
+  [ ! "$username" == "null" ] && CLI_ENV="CHECKHTTP_HTTP_USER=${username}"
+  [ ! "$password" == "null" ] && CLI_ENV="${CLI_ENV} CHECKHTTP_HTTP_PASS=${password}"
 fi
 
-exec python /app/checkhttp.py
+eval ${CLI_ENV} python checkhttp.py
