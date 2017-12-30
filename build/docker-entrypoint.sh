db714b82bbe380df33ba19c1199dd84a5928f531
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-12-30 10:16:04 +0100

    add docker secrets env load

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function load_secret {
+    # function checks if specified environment variable contains the file path to a docker secret
+    # if so it overwrite the value with the file contents
+
+    # first parameter is the environment variable name 
+    name=${1}
+    # second parameter is the value of the environment variable
+    value=${2}
+
+    # now check if the value equals a file in the container
+    if [ -f "${value}" ]; then
+        export ${name}=$(cat "${value}")
+    fi
+}
+
+CLI_ENV=""
 # if vault is enabled try to access it and get the username and password
 if [ -n "$VAULT_SERVER" ]; then
   ACCESS_TOKEN=$(curl -X POST \
@@ -12,9 +28,14 @@ if [ -n "$VAULT_SERVER" ]; then
   username=$(echo $json | jq -r .data.username)
   password=$(echo $json | jq -r .data.password)
 
-  CLI_ENV=""
-  [ ! "$username" == "null" ] && CLI_ENV="CHECKHTTP_HTTP_USER=${username}"
-  [ ! "$password" == "null" ] && CLI_ENV="${CLI_ENV} CHECKHTTP_HTTP_PASS=${password}"
+  [ ! "$username" == "null" ] && export CHECKHTTP_HTTP_USER="${username}"
+  [ ! "$password" == "null" ] && export CHECKHTTP_HTTP_PASS="${password}"
+
+else
+  # lets see if we have the username/password specified in a docker secret
+  load_secret CHECKHTTP_HTTP_USER "${CHECKHTTP_HTTP_USER}"
+  load_secret CHECKHTTP_HTTP_PASS "${CHECKHTTP_HTTP_PASS}"
 fi
 
-eval ${CLI_ENV} python checkhttp.py
+
+exec python checkhttp.py
